0a48aec795e3b08986b1e9c779ea161b47a6befa
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2022-07-06 16:30:48 -0700

    Update zebrafish_imaging.py

--- scripts/zebrafish_imaging.py
+++ scripts/zebrafish_imaging.py
@@ -23,6 +23,9 @@ z_position_list = [mm_pos_list.get_position(i).get('ZDrive').get1_d_position()
 
 #%% define imaging settings
 
+data_directory = r''
+data_name = ''
+
 # fluorescence channels
 epi_channels = [{'group': 'Master Channel', 'config': 'Epi-GFP'},
                 {'group': 'Master Channel', 'config': 'Epi-DSRED'}]
@@ -53,7 +56,6 @@ autofocus_manager.set_autofocus_method_by_name('PFS')
 #%% generate acquisition events
 
 events = []
-
 for t_idx in range(num_time_points):
     for p_idx, p in enumerate(xy_position_list):
 
@@ -68,37 +70,58 @@ for t_idx in range(num_time_points):
                     'z': z,
                     'channel': channel,
                     'exposure': exp,
-                    'keep_shutter_open': True,
+                    'keep_shutter_open': False  # keep shutter closed to disable sequencing here
                 })
 
-    # epi and lf positions
-    for p_idx, p in enumerate(lf_epi_positions):
-
-        # epi channel, z-first
-        for z_idx, z in enumerate(z_range + p[2]):
-            events.append(
-                {
-                    'axes': {'time': t, 'position': p_idx + num_epi_positions, 'z': z_idx},
-                    'min_start_time': t * time_interval_s,
+        # lf positions, channel first
+        for z_idx, z in enumerate(z_range):
+            for channel in lf_channels:
+                events.append({
+                    'axes': {'time': t_idx, 'position': p_idx, 'z': z_idx},
+                    'min_start_time': t_idx * time_interval_s,
                     'x': p[0],
                     'y': p[1],
                     'z': z,
-                    'channel': epi_channel,
-                    'exposure': epi_exposure,
-                    'keep_shutter_open': False
+                    'channel': channel,
+                    'exposure': lf_exposure,
+                    'keep_shutter_open': True
                 })
 
-        # lf channels, channel-first
-        for z_idx, z in enumerate(z_range + p[2]):
-            for ch in lf_channels:
-                events.append(
-                    {
-                        'axes': {'time': t, 'position': p_idx + num_epi_positions, 'z': z_idx},
-                        'min_start_time': t * time_interval_s,
-                        'x': p[0],
-                        'y': p[1],
-                        'z': z,
-                        'channel': ch,
-                        'exposure': lf_exposure,
-                        'keep_shutter_open': False
-                    })
+#%% hook functions
+
+def post_hardware_hook(event, bridge, event_queue):
+
+    if 'axes' in event.keys():  # some events only modify hardware
+        if not hasattr(post_hardware_hook, 'pos_index'):
+            post_hardware_hook.pos_index = -1
+
+        # Only run hook function at new positions (including the first one)
+        pos_index = event['axes']['position']
+        if pos_index != post_hardware_hook.pos_index:
+            # get the Micro-Manager core
+            mmc = bridge.get_core()
+
+            # apply ZDrive position
+            mmc.set_position('ZDrive', z_position_list[pos_index])
+
+        # Keep track of last time point on which autofocus ran
+        post_hardware_hook.pos_index = pos_index
+
+    return event
+
+#%%  acquire data
+
+# turn on autoshutter
+mmc.set_auto_shutter(True)
+
+# turn off live preview
+snap_live_manager = mmStudio.get_snap_live_manager()
+if snap_live_manager.is_live_mode_on():
+    snap_live_manager.set_live_mode_on(False)
+
+# acquire data
+with Acquisition(directory=data_directory, name=data_name) as acq:
+    acq.acquire(events)
+
+# with Acquisition(directory=data_directory, name=data_name, post_camera_hook_fn=post_hardware_hook) as acq:
+#     acq.acquire(events)
