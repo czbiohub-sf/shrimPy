9cf5823660dc1aad00bbb1b6c934fe3450829a8d
Rachel Banks <rachel.banks@locals-MacBook-Pro-5.local>
2022-08-08 15:47:32 -0700

    fixed time index issue

--- scripts/zebrafish_autotracker.py
+++ scripts/zebrafish_autotracker.py
@@ -40,14 +40,9 @@ def img_process_fn(image, metadata, bridge, event_queue):
             #img_process_fn.images = []
             print(metadata)
 
-            if time_index > 0: 
+            if time_index > 1: 
 
-                if channel == 'DAPI':
-                # calculate the shift in x,y, and z
-                # right now, just picking the image in the middle of the stack, need to think more about this
-                # ref_image = img_process_fn.images[4]
-                # curr_image = img_process_fn.images[int((time_index)*10+4)]
-                # compare with the first time point
+                if channel == 'FITC':
                     
                     x_pos = metadata['XPosition_um_Intended']
                     y_pos = metadata['YPosition_um_Intended']
@@ -70,28 +65,30 @@ def img_process_fn(image, metadata, bridge, event_queue):
                     channels = ['DAPI', 'FITC']
                     event = []
                     for c_index, channel in enumerate(channels):
-                        for index, z_um in enumerate(np.arange(start=0 + dz, stop=10 + dz, step=1)):
+                        for index, z_um in enumerate(np.arange(start=0, stop=10, step=1)):
                             evt = {
                                 'axes' : {'z': index, 'time': time_index, 'position': 0},
-                                'x' : x_pos+ dx,
-                                'y' : y_pos + dy,
+                                'x' : 0 + dx,
+                                'y' : 0 + dy,
                                 'z' : z_um,
-                                'channel' : channel
+                                'channel' : {'group' : 'Channel', 'config' : channel}
                             }
                             event.append(evt)
-                    
                     event_queue.put(event)
 
-                # event = multi_d_acquisition_events(
-                #                     num_time_points=1, time_interval_s=0,
-                #                     channel_group='Channel', channels=['DAPI', 'FITC'],
-                #                     z_start=0, z_end=10, z_step=1,
-                #                     order='tcz')
+                    # event = multi_d_acquisition_events(
+                    #                 num_time_points=1, time_interval_s=0,
+                    #                 channel_group='Channel', channels=['DAPI', 'FITC'],
+                    #                 xy_positions = [0 + dx,0 + dy],
+                    #                 z_start=0, z_end=9, z_step=1,
+                    #                 order='tcz')
+                    
+                    # event_queue.put(event)
             
                 
             
             else:
-                print('time index is zero')
+               
                 time_index += 1
                 # define the next event and add to the queue
                 channels = ['DAPI', 'FITC']
@@ -108,11 +105,13 @@ def img_process_fn(image, metadata, bridge, event_queue):
                         event.append(evt)
                 event_queue.put(event)
 
-                #event = multi_d_acquisition_events(
-                                   # num_time_points=1, time_interval_s=0,
-                                   # channel_group='Channel', channels=['DAPI', 'FITC'],
-                                   # z_start=0, z_end=10, z_step=1,
-                                  #  order='tcz')
+                # event = multi_d_acquisition_events(
+                #                    num_time_points=1, time_interval_s=0,
+                #                    channel_group='Channel', channels=['DAPI', 'FITC'],
+                #                   # xy_positions = [0,0],
+                #                    z_start=0, z_end=9, z_step=1,
+                #                    order='tcz')
+                # event_queue.put(event)
                 
                         
                 
@@ -150,7 +149,8 @@ for c_index, channel in enumerate(epi_channels):
 # events = multi_d_acquisition_events(
 #                                     num_time_points=2, time_interval_s=0,
 #                                     channel_group='Channel', channels=['DAPI', 'FITC'],
-#                                     z_start=0, z_end=10, z_step=1,
+#                                    # xy_positions = [0,0],
+#                                     z_start=0, z_end=9, z_step=1,
 #                                     order='tcz')
 
 acq = Acquisition(directory='/Users/rachel.banks/Documents/pycromanager_tests/', name='test',
