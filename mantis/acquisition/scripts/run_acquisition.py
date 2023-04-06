e153809fbf6b01b97400808fd3ab68dd8c7c05d2
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2023-04-06 11:55:32 -0700

    Revert "edited acquisition cli and updated docstrings"
    This reverts commit 6c9fda4d07d0b27dee32175375f29e7f3b662284.

--- mantis/acquisition/scripts/run_acquisition.py
+++ mantis/acquisition/scripts/run_acquisition.py
@@ -9,41 +9,40 @@ from mantis.acquisition.BaseSettings import (
     MicroscopeSettings,
 )
 
-
 @click.command()
 @click.help_option("-h", "--help")
 @click.option(
-    "--data-dirpath",
-    required=True,
-    type=click.Path(exists=True, file_okay=False, dir_okay=True),
-    help="Directory where acquired data will be saved",
+    '--data-dirpath', 
+    required=True, 
+    type=click.Path(exists=True),
+    help='Directory where acquired data will be saved',
 )
 @click.option(
-    "--name",
+    '--name', 
     required=True,
-    help="Name of the acquisition",
+    help='Name of the acquisition',
 )
 @click.option(
-    "--settings",
-    required=True,
+    '--settings', 
+    required=True, 
     type=click.Path(exists=True, file_okay=True, dir_okay=False),
-    help="YAML file containing the acquisition settings",
+    help='YAML file containing the acquisition settings',
 )
 @click.option(
-    "--mm-app-path",
-    default="C:\\Program Files\\Micro-Manager-nightly",
-    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    '--mm-app-path', 
+    default='C:\\Program Files\\Micro-Manager-nightly', 
+    type=click.Path(exists=True),
     show_default=True,
-    help="""Path to Micro-manager installation directory
-      which will run the light-sheet acquisition""",
+    help='''Path to Micro-manager installation directory
+      which will run the light-sheet acquisition''',
 )
 @click.option(
-    "--mm-config-file",
-    default="C:\\CompMicro_MMConfigs\\mantis\\mantis-LS.cfg",
+    '--mm-config-file', 
+    default='C:\\CompMicro_MMConfigs\\mantis\\mantis-LS.cfg', 
     type=click.Path(exists=True, file_okay=True, dir_okay=False),
     show_default=True,
-    help="""Path to Micro-manager config file 
-      which will run the light-sheet acquisition""",
+    help='''Path to Micro-manager config file 
+      which will run the light-sheet acquisition''',
 )
 def run_acquisition(
     data_dirpath,
@@ -52,10 +51,9 @@ def run_acquisition(
     mm_app_path,
     mm_config_file,
 ):
-    """Command-line utility to acquire label-free and light-sheet images on mantis."""
 
-    demo_run = True if "demo" in mm_config_file else False
-
+    demo_run = True if 'demo' in mm_config_file else False
+    
     with open(settings) as file:
         raw_settings = yaml.safe_load(file)
 
@@ -68,30 +66,26 @@ def run_acquisition(
         verbose=False,
     ) as acq:
 
-        acq.time_settings = TimeSettings(**raw_settings.get("time_settings"))
-        acq.position_settings = PositionSettings()
-        acq.lf_acq.channel_settings = ChannelSettings(
-            **raw_settings.get("lf_channel_settings")
-        )
-        acq.lf_acq.slice_settings = SliceSettings(
-            **raw_settings.get("lf_slice_settings")
-        )
-        acq.lf_acq.microscope_settings = MicroscopeSettings(
-            **raw_settings.get("lf_microscope_settings")
-        )
-        acq.ls_acq.channel_settings = ChannelSettings(
-            **raw_settings.get("ls_channel_settings")
-        )
-        acq.ls_acq.slice_settings = SliceSettings(
-            **raw_settings.get("ls_slice_settings")
-        )
-        acq.ls_acq.microscope_settings = MicroscopeSettings(
-            **raw_settings.get("ls_microscope_settings")
-        )
-
+        acq.time_settings = \
+            TimeSettings(**raw_settings.get('time_settings'))
+        acq.position_settings = \
+            PositionSettings()
+        acq.lf_acq.channel_settings = \
+            ChannelSettings(**raw_settings.get('lf_channel_settings'))
+        acq.lf_acq.slice_settings = \
+            SliceSettings(**raw_settings.get('lf_slice_settings'))
+        acq.lf_acq.microscope_settings = \
+            MicroscopeSettings(**raw_settings.get('lf_microscope_settings'))
+        acq.ls_acq.channel_settings = \
+            ChannelSettings(**raw_settings.get('ls_channel_settings'))
+        acq.ls_acq.slice_settings = \
+            SliceSettings(**raw_settings.get('ls_slice_settings'))
+        acq.ls_acq.microscope_settings = \
+            MicroscopeSettings(**raw_settings.get('ls_microscope_settings'))
+        
         acq.setup()
         acq.acquire()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run_acquisition()
