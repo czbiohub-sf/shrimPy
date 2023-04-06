5fa862c2ddd0323583053d5620ee2ccd02da84c8
Uditha Velidandla <ivan.ivanov@czbiohub.org>
2023-04-06 14:35:03 -0700

    move run_acquisition to cli module

--- mantis/cli/run_acquisition.py
+++ mantis/cli/run_acquisition.py
@@ -57,6 +57,16 @@ def run_acquisition(
     with open(settings) as file:
         raw_settings = yaml.safe_load(file)
 
+    # Load and validate YAML settings
+    time_settings = TimeSettings(**raw_settings.get('time_settings'))
+    position_settings = PositionSettings()
+    lf_channel_settings = ChannelSettings(**raw_settings.get('lf_channel_settings'))
+    lf_slice_settings = SliceSettings(**raw_settings.get('lf_slice_settings'))
+    lf_microscope_settings = MicroscopeSettings(**raw_settings.get('lf_microscope_settings'))
+    ls_channel_settings = ChannelSettings(**raw_settings.get('ls_channel_settings'))
+    ls_slice_settings = SliceSettings(**raw_settings.get('ls_slice_settings'))
+    ls_microscope_settings = MicroscopeSettings(**raw_settings.get('ls_microscope_settings'))
+
     with MantisAcquisition(
         acquisition_directory=data_dirpath,
         acquisition_name=name,
@@ -66,22 +76,14 @@ def run_acquisition(
         verbose=False,
     ) as acq:
 
-        acq.time_settings = \
-            TimeSettings(**raw_settings.get('time_settings'))
-        acq.position_settings = \
-            PositionSettings()
-        acq.lf_acq.channel_settings = \
-            ChannelSettings(**raw_settings.get('lf_channel_settings'))
-        acq.lf_acq.slice_settings = \
-            SliceSettings(**raw_settings.get('lf_slice_settings'))
-        acq.lf_acq.microscope_settings = \
-            MicroscopeSettings(**raw_settings.get('lf_microscope_settings'))
-        acq.ls_acq.channel_settings = \
-            ChannelSettings(**raw_settings.get('ls_channel_settings'))
-        acq.ls_acq.slice_settings = \
-            SliceSettings(**raw_settings.get('ls_slice_settings'))
-        acq.ls_acq.microscope_settings = \
-            MicroscopeSettings(**raw_settings.get('ls_microscope_settings'))
+        acq.time_settings = time_settings
+        acq.position_settings = position_settings
+        acq.lf_acq.channel_settings = lf_channel_settings
+        acq.lf_acq.slice_settings = lf_slice_settings
+        acq.lf_acq.microscope_settings = lf_microscope_settings
+        acq.ls_acq.channel_settings = ls_channel_settings
+        acq.ls_acq.slice_settings = ls_slice_settings
+        acq.ls_acq.microscope_settings = ls_microscope_settings
         
         acq.setup()
         acq.acquire()
