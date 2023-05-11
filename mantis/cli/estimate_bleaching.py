d0eb56041ca2852207cf420c51266b6607472e3f
Talon Chandler <talonchandler@gmail.com>
2023-05-10 17:16:52 -0700

    early template

---
+++ mantis/cli/estimate_bleaching.py
@@ -0,0 +1,30 @@
+import click
+import matplotlib.pyplot as plt
+
+from iohub import read_micromanager
+
+
+@click.command()
+@click.argument(
+    "data_path",
+    type=click.Path(exists=True),
+)
+@click.option(
+    "--output-file",
+    "-o",
+    default="./bleaching.pdf",
+    required=False,
+    help="Path to saved parameters",
+)
+def estimate_bleaching(data_path, output_file):
+    """Estimate bleaching from raw data"""
+    # Read data
+    reader = read_micromanager(data_path)
+    import pdb
+
+    pdb.set_trace()
+    data = reader.get_array(0)[0, 0, ...]  # zyx
+
+    # Collect statistics for each time point
+
+    # Generate and save plot
--- mantis/cli/main.py
+++ mantis/cli/main.py
@@ -1,6 +1,7 @@
 import click
 
 from mantis.cli.deskew import deskew
+from mantis.cli.estimate_bleaching import estimate_bleaching
 from mantis.cli.estimate_deskew import estimate_deskew
 from mantis.cli.run_acquisition import run_acquisition
 
@@ -11,5 +12,6 @@ def cli():
 
 
 cli.add_command(deskew)
+cli.add_command(estimate_bleaching)
 cli.add_command(estimate_deskew)
 cli.add_command(run_acquisition)
