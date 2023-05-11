010c5931d0cc5672195c78a16c201fcf5b7f5b70
Talon Chandler <talonchandler@gmail.com>
2023-05-10 17:40:27 -0700

    read timestamp

--- mantis/cli/estimate_bleaching.py
+++ mantis/cli/estimate_bleaching.py
@@ -22,6 +22,10 @@ def estimate_bleaching(data_path, output_file):
     reader = read_micromanager(data_path)
     import pdb
 
+    for p in range(reader.get_num_positions()):
+        for t in range(reader.shape[0]):
+            for c in range(reader.shape[1]):
+                time = reader.get_image_metadata(p, t, c, 0)["TimeStampMsec"]
     pdb.set_trace()
     data = reader.get_array(0)[0, 0, ...]  # zyx
 
