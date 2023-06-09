ed8bb2e31a5a3e8aed7d6e9e9e70f8106973f320
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2023-06-09 16:26:08 -0700

    cleaning up the deskew cli function.

--- mantis/cli/deskew.py
+++ mantis/cli/deskew.py
@@ -18,8 +18,8 @@ from mantis.cli.parsing import (
     deskew_param_argument,
     output_dataset_options,
 )
-
-N_processes = 16
+from time import time
+from natsort import natsorted
 
 
 def _get_deskew_params(deskew_params_path):
@@ -94,6 +94,26 @@ def _get_output_paths(list_pos, output_path):
     return list_output_path
 
 
+def single_process(data_array, output_path, settings, keep_overhang, t, c):
+    click.echo(f"Deskewing c={c}, t={t}")
+    click.echo(f'data_array.shape {data_array[0][t, c].shape}')
+    start_time = time()
+    data = data_array[0][t, c]
+
+    click.echo(f'total time {time()-start_time}')
+
+    # Deskew
+    deskewed = deskew_data(
+        data, settings.px_to_scan_ratio, settings.ls_angle_deg, keep_overhang, verbose=True
+    )
+    click.echo(f"Writing.. c={c}, t={t}")
+    # Write to file
+    with open_ome_zarr(output_path, mode="r+") as output_dataset:
+        output_dataset[0][t, c] = deskewed
+
+    click.echo(f"Finished Writing.. c={c}, t={t}")
+
+
 def deskew_cli(
     input_data_path, output_path, deskew_params_path, view, keep_overhang, num_cores
 ):
@@ -102,14 +122,14 @@ def deskew_cli(
     """
 
     # Get the reader and writer
-    print(f'Input data path:\t{input_data_path}')
-    print(f'Output data path:\t{str(output_path)}')
+    click.echo(f'Input data path:\t{input_data_path}')
+    click.echo(f'Output data path:\t{str(output_path)}')
     input_dataset = open_ome_zarr(str(input_data_path))
-    print(input_dataset.print_tree())
+    click.echo(input_dataset.print_tree())
 
     settings = _get_deskew_params(deskew_params_path)
     T, C, Z, Y, X = input_dataset.data.shape
-    print(f'Dataset shape:\t{input_dataset.data.shape}')
+    click.echo(f'Dataset shape:\t{input_dataset.data.shape}')
 
     deskewed_shape, voxel_size = get_deskewed_data_shape(
         (Z, Y, X),
@@ -120,22 +140,17 @@ def deskew_cli(
     )
 
     # Loop through (T, C), deskewing and writing as we go
-    with mp.Pool(mp.cpu_count()) as p:
+    click.echo(f"Starting multiprocess pool with cores {num_cores}")
+    with mp.Pool(num_cores) as p:
         p.starmap(
-            partial(
-                single_process,
-                input_dataset,
-                str(output_path),
-                settings,
-                keep_overhang,
-            ),
+            partial(single_process, input_dataset, str(output_path), settings, keep_overhang),
             itertools.product(range(T), range(C)),
         )
     # TODO: check if output_path[0] will work when we parallelize
-
     # Write metadata
     output_zarr_root = str(output_path).split(os.path.sep)[:-3]
     output_zarr_root = os.path.join(*output_zarr_root)
+    click.echo(f'output_zarr_root \t{output_zarr_root}')
     with open_ome_zarr(output_zarr_root, mode='r+') as dataset:
         dataset.zattrs["deskewing"] = asdict(settings)
         # TODO: not sure what this metadata was for
@@ -144,6 +159,7 @@ def deskew_cli(
     # Optional view
     # TODO: implement the viewer capability
     if view:
+        click.echo(f"View mode activated")
         viewer = napari.Viewer()
         napari.run()
     else:
@@ -153,47 +169,6 @@ def deskew_cli(
     #     curr_layer = 'deskew_' + str(t) + 'T_' + str(c) + 'C'
     #     viewer.add_image(deskewed, name=curr_layer)
     #     viewer.layers[curr_layer].scale = voxel_size
-
-
-def single_process(data_array, output_path, settings, keep_overhang, t, c):
-    print(f"Deskewing c={c}, t={t}")
-
-    # Deskew
-    deskewed = deskew_data(
-        data_array[0][t, c], settings.px_to_scan_ratio, settings.ls_angle_deg, keep_overhang
-    )
-
-    # Write to file
-    with open_ome_zarr(output_path, mode="r+") as output_dataset:
-        output_dataset[0][t, c] = deskewed
-
-
-def parallel(func=None, **options):
-    """
-    Decorator for multiprocessing Pool
-    """
-    if func is None:
-        return partial(parallel, **options)
-
-    def wrapper(*arguments):
-        # Split the arguments
-        position_paths = arguments[0]
-        output_paths = arguments[1]
-        deskew_params = arguments[2:]
-
-        # Check number of processes and available cores
-        processes = mp.cpu_count() if arguments[-1] > mp.cpu_count() else arguments[-1]
-        # Multiprocess per position
-        result = []
-        with mp.Pool(processes) as pool:
-            for pos_in, pos_out in zip(position_paths, output_paths):
-                args = (pos_in,) + (pos_out,) + deskew_params
-                print(f' aruuuments {args}')
-                for results in tqdm(pool.starmap(func, [args])):
-                    result.append(results)
-        return result
-
-    return wrapper
 
 
 @click.command()
@@ -230,9 +205,11 @@ def parallel(func=None, **options):
     is_flag=True,
     help="Using slurm",
 )
-def deskew(input_data_path, deskew_param_path, output_path, view, keep_overhang, num_cores, slurm):
+def deskew(
+    input_data_path, deskew_param_path, output_path, view, keep_overhang, num_cores, slurm
+):
     "Deskews a single position across T and C axes using a parameter file generated by estimate_deskew.py"
-
+    input_data_path = natsorted(input_data_path)
     # Handle single position or wildcard filepath
     list_output_pos = _get_output_paths(input_data_path, output_path)
     print(f'List of input pos:{input_data_path} output_pos:{list_output_pos}')
@@ -242,7 +219,7 @@ def deskew(input_data_path, deskew_param_path, output_path, view, keep_overhang,
         _create_empty_zarr(input_data_path, deskew_param_path, output_path, keep_overhang)
 
     # Parallel Position
-    mp_position_deskew = parallel(deskew_cli)
+    # mp_position_deskew = parallel(deskew_cli)
     deskew_parameters = (
         input_data_path,
         list_output_pos,
@@ -258,6 +235,3 @@ def deskew(input_data_path, deskew_param_path, output_path, view, keep_overhang,
     for pos_in, pos_out in zip(input_data_path, list_output_pos):
         args = (pos_in,) + (pos_out,) + deskew_params
         deskew_cli(*args)
-
-    # TODO: nested multiprocessing is not good practice and causes errors.
-    # mp_position_deskew(*deskew_parameters)
