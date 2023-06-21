9d35195e67845bc61f5b0029879de7cfd8dd02cb
Talon Chandler <talonchandler@gmail.com>
2023-06-21 12:00:03 -0700

    initial cleaning of example script

--- examples/slurmkit_example/slurm_deskew.py
+++ examples/slurmkit_example/slurm_deskew.py
@@ -3,7 +3,6 @@ import os
 import glob
 from mantis.cli.deskew import (
     deskew_cli,
-    get_deskew_params,
     create_empty_zarr,
     get_output_paths,
 )
@@ -11,38 +10,31 @@ from slurmkit import SlurmParams, slurm_function, submit_function
 from natsort import natsorted
 import click
 
-# TODO: decide what logging we use (i.e click.echo, print, logging)
+# deskew parameters
+keep_overhang = False
 
-## PYTHON SCRIPT GLOBAL PARAMETERS
+# io parameters
 input_data_path = '/hpc/projects/comp.micro/mantis/2023_05_10_PCNA_RAC1/0-crop-convert-zarr/sample_short.zarr/*/*/*'
 output_data_path = './deskewed.zarr'
 deskew_param_path = './deskew_settings.yml'
-keep_overhang = False
 
-## SLURM SBATCH GLOBAL PARAMETERS
+# sbatch parameters
 cpu_per_task = 16
 mem = "16G"
-time = 40
+time = 40  # minutes
 
-# ---------------INITIALIZATION AND DATA PARSING ----------------------
-# Deskewing I/O
+# path handling
 input_data_path = glob.glob(input_data_path)
 input_data_path = natsorted(input_data_path)
 output_dir = os.path.dirname(os.path.join(os.getcwd(), output_data_path))
 list_output_pos = get_output_paths(input_data_path, output_data_path)
 click.echo(f"in: {input_data_path}, out: {list_output_pos}")
-
-# Slurm I/O
 slurm_out_path = str(os.path.join(output_dir, "slurm_output/deskew-%j.out"))
 
-# Initialize the array
+# initialize zarr
 create_empty_zarr(input_data_path, deskew_param_path, output_data_path, keep_overhang)
 
-# -------------------- SLURM PARAMETERS ----------------------------------
-
-# Slurm Parameters for each node
-# NOTE: These parameters have to be tunned depending on the size of the job
-# These parameters could be reused by multiple jobs. The job-name by default takes the slurm_function's name
+# prepare slurm parameters
 params = SlurmParams(
     partition="cpu",
     cpus_per_task=cpu_per_task,
@@ -51,13 +43,13 @@ params = SlurmParams(
     output=slurm_out_path,
 )
 
-# Wrap our deskew_cli() function with slurmkit
+# wrap our deskew_cli() function with slurmkit
 slurm_deskew_cli = slurm_function(deskew_cli)
 deskew_func = slurm_deskew_cli(
     deskew_param_path=deskew_param_path, keep_overhang=keep_overhang
 )
 
-# Generates the array of jobs by passing the in_path and out_path to slurm wrapped function
+# generate an array of jobs by passing the in_path and out_path to slurm wrapped function
 deskew_jobs = [
     submit_function(
         deskew_func,
