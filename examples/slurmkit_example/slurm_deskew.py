2d7c317cedeb6ecfa06948ec4e06d8ed3534f6d4
Talon Chandler <talonchandler@gmail.com>
2023-06-21 14:36:42 -0700

    match names

--- examples/slurmkit_example/slurm_deskew.py
+++ examples/slurmkit_example/slurm_deskew.py
@@ -14,7 +14,7 @@ import click
 keep_overhang = False
 
 # io parameters
-input_data_path = '/hpc/projects/comp.micro/mantis/2023_05_10_PCNA_RAC1/timelapse_2_3/0-crop-convert-zarr/sample_short.zarr/*/*/*'
+input_paths = '/hpc/projects/comp.micro/mantis/2023_05_10_PCNA_RAC1/timelapse_2_3/0-crop-convert-zarr/sample_short.zarr/*/*/*'
 output_data_path = './deskewed.zarr'
 deskew_param_path = './deskew_settings.yml'
 
@@ -24,15 +24,15 @@ mem = "16G"
 time = 40  # minutes
 
 # path handling
-input_data_path = glob.glob(input_data_path)
-input_data_path = natsorted(input_data_path)
+input_paths = glob.glob(input_paths)
+input_paths = natsorted(input_paths)
 output_dir = os.path.dirname(os.path.join(os.getcwd(), output_data_path))
-list_output_pos = get_output_paths(input_data_path, output_data_path)
-click.echo(f"in: {input_data_path}, out: {list_output_pos}")
+output_paths = get_output_paths(input_paths, output_data_path)
+click.echo(f"in: {input_paths}, out: {output_paths}")
 slurm_out_path = str(os.path.join(output_dir, "slurm_output/deskew-%j.out"))
 
 # initialize zarr
-create_empty_zarr(input_data_path, deskew_param_path, output_data_path, keep_overhang)
+create_empty_zarr(input_paths, deskew_param_path, output_data_path, keep_overhang)
 
 # prepare slurm parameters
 params = SlurmParams(
@@ -48,6 +48,7 @@ slurm_deskew_cli = slurm_function(deskew_single_position)
 deskew_func = slurm_deskew_cli(
     deskew_param_path=deskew_param_path, keep_overhang=keep_overhang
 )
+
 # generate an array of jobs by passing the in_path and out_path to slurm wrapped function
 deskew_jobs = [
     submit_function(
@@ -56,5 +57,5 @@ deskew_jobs = [
         input_data_path=in_path,
         output_path=out_path,
     )
-    for in_path, out_path in zip(input_data_path, list_output_pos)
+    for in_path, out_path in zip(input_paths, output_paths)
 ]
