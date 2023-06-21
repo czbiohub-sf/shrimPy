e75eb99f32ad76653a90348874e948f0ebadc973
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2023-06-21 16:36:50 -0700

    cleanup the demo code

--- mantis/analysis/drift_correction.py
+++ mantis/analysis/drift_correction.py
@@ -2,24 +2,31 @@
 from copylot.hardware.stages.thorlabs.KIM001 import KCube_PiezoInertia
 import time
 from copylot import logger
+from waveorder.focus import focus_from_transverse_band
 
-### LABEL_FREE STAGE
-# with KCube_PiezoInertia(serial_number='74000565', simulator=False) as stage_LF:
-    # print(f'LF current position {stage_LF.position}')
-    # stage_LF.move_relative(10)
 
-### LIGHT SHEET STAGE
-with KCube_PiezoInertia(serial_number='74000291', simulator=False) as stage_LS:
-    # Test the relative movement
-    print(f'LS current position {stage_LS.position}')
-    stage_LS.move_relative(100)
-    stage_LS.move_relative(-100)
+def test_labelfree_stage():
+    with KCube_PiezoInertia(serial_number='74000565', simulator=False) as stage_LF:
+        print(f'LF current position {stage_LF.position}')
+        stage_LF.move_relative(10)
 
-    # Change the acceleration and step rate
-    stage_LS.step_rate = 50
-    stage_LS.step_acceleration = 200
-    print(f'acceleration {stage_LS.step_acceleration} rate {stage_LS.step_rate}')
-    # Test the movement with the different acceleration
-    stage_LS.move_relative(100)
-    stage_LS.move_relative(-100)
 
+def test_light_sheet_stage():
+    ### LIGHT SHEET STAGE
+    with KCube_PiezoInertia(serial_number='74000291', simulator=False) as stage_LS:
+        # Test the relative movement
+        print(f'LS current position {stage_LS.position}')
+        stage_LS.move_relative(100)
+        stage_LS.move_relative(-100)
+
+        # Change the acceleration and step rate
+        stage_LS.step_rate = 50
+        stage_LS.step_acceleration = 200
+        print(f'acceleration {stage_LS.step_acceleration} rate {stage_LS.step_rate}')
+        # Test the movement with the different acceleration
+        stage_LS.move_relative(100)
+        stage_LS.move_relative(-100)
+
+
+if __name__ == '__main__':
+    test_light_sheet_stage()
