5721485e1679d669eb0aeda75fea2958575efb33
LabelFree <ivan.ivanov@czbiohub.org>
2023-06-30 17:43:55 -0700

    Update acquire_defocus_stack.py

--- examples/acquire_defocus_stack.py
+++ examples/acquire_defocus_stack.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pycromanager import Core, Studio
 from mantis.acquisition.microscope_operations import setup_kim101_stage, acquire_ls_defocus_stack
 
@@ -8,16 +9,19 @@ z_end = 100
 z_step = 10
 config_group = 'Channel - LS'
 config_name = 'GFP EX488 EM525-45'
+galvo = 'AP Galvo'
+galvo_range = [-1, 0, 1]
 
 z_stage = setup_kim101_stage('74000291')
+z_range = np.arange(z_start, z_end + z_step, z_step)
 
 data = acquire_ls_defocus_stack(
     mmc, 
     mmStudio, 
     z_stage, 
-    z_start, 
-    z_end, 
-    z_step, 
+    z_range,
+    galvo,
+    galvo_range,
     config_group, 
     config_name,
 )
