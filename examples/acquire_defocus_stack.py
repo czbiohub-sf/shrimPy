cbf624de60c2b7e15847017e0d2859e59dcb72df
LabelFree <ivan.ivanov@czbiohub.org>
2023-06-30 17:29:07 -0700

    update examples

--- examples/acquire_defocus_stack.py
+++ examples/acquire_defocus_stack.py
@@ -1,13 +1,25 @@
 from pycromanager import Core, Studio
-from mantis.acquisition.microscope_operations import acquire_ls_defocus_stack
+from mantis.acquisition.microscope_operations import setup_kim101_stage, acquire_ls_defocus_stack
 
 mmc = Core()
 mmStudio = Studio()
-z_stage = 'Z'
-z_start = 0
-z_end = 10
-z_step = 1
+z_start = 10
+z_end = 100
+z_step = 10
+config_group = 'Channel - LS'
+config_name = 'GFP EX488 EM525-45'
 
-data = acquire_ls_defocus_stack(mmc, mmStudio, z_stage, z_start, z_end, z_step)
+z_stage = setup_kim101_stage('74000291')
+
+data = acquire_ls_defocus_stack(
+    mmc, 
+    mmStudio, 
+    z_stage, 
+    z_start, 
+    z_end, 
+    z_step, 
+    config_group, 
+    config_name,
+)
 
 print(data.shape)
--- examples/test_kim101_stage.py
+++ examples/test_kim101_stage.py
@@ -15,17 +15,15 @@ def test_light_sheet_stage():
     ### LIGHT SHEET STAGE
     with KCube_PiezoInertia(serial_number='74000291', simulator=False) as stage_LS:
 
-        print(f'LS current position {stage_LS.position}')
-
         # Change the acceleration and step rate
         stage_LS.step_rate = 500
         stage_LS.step_acceleration = 1000
-        print(f'acceleration {stage_LS.step_acceleration} rate {stage_LS.step_rate}')
 
         # Test relative movement
-        step_size = 10
-        stage_LS.move_relative(step_size)
-        stage_LS.move_relative(-step_size)
+        step_size = -10
+        for i in range(10):
+            stage_LS.move_relative(10)
+        # stage_LS.move_relative(-step_size)
 
 
 if __name__ == '__main__':
