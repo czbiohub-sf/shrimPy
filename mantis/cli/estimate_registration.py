6e3543773e8664ab8d7d20ff2005cedb2c934073
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2023-07-20 12:19:03 -0700

    adding voxel_size

--- mantis/cli/estimate_registration.py
+++ mantis/cli/estimate_registration.py
@@ -1,4 +1,3 @@
-# %%
 import os
 
 
@@ -8,12 +7,61 @@ import numpy as np
 import scipy
 
 from iohub import open_ome_zarr
-from waveorder.focus import focus_from_transverse_band
-
+from pathlib import Path
 from skimage.transform import SimilarityTransform
+from waveorder.focus import focus_from_transverse_band
 
 # TODO: remove this for non mantis datasets
 ROTATE_90deg_CCW = True
+# TODO: add option to use the autofocused slice
+
+
+def find_focus_channel_pairs(
+    channel_1_data_path: Path,
+    channel_1_idx: int,
+    channel_2_data_path: Path,
+    channel_2_idx: int,
+):
+    """
+    Find the focus sliced for input channels
+
+    Parameters
+    ----------
+    channel_1_data_path : Path
+    channel_1_idx : int
+    channel_2_data_path : Path
+    channel_2_idx : int
+    """
+    # TODO: get the microscope parameters through a yaml file? Currently hard-coded
+    # Find focus channel_1
+    print("Finding the channel_1 best focus position at t=0")
+    with open_ome_zarr(channel_1_data_path, mode="r") as channel_1_position:
+        channel_1_str = channel_1_position.channel_names[channel_1_idx]
+        focus_channel_1_idx = focus_from_transverse_band(
+            channel_1_position[0][0, channel_1_idx],
+            NA_det=1.35,
+            lambda_ill=0.55,
+            pixel_size=3.45 / (46.2 * 1.4),
+        )
+        channel_1_Z, channel_1_Y, channel_1_X = channel_1_position[0][
+            0, channel_1_idx
+        ].shape
+        print(f"{channel_1_str} focus idx: {focus_channel_1_idx}")
+
+    # Find focus channel_2
+    print("Finding the channel_2 best focus position at t=0")
+    with open_ome_zarr(channel_2_data_path, mode="r") as channel_2_position:
+        channel_2_position.channel_names[channel_2_idx]
+        focus_channel_2_idx = focus_from_transverse_band(
+            channel_2_position[0][0, channel_2_idx],
+            NA_det=1.35,
+            lambda_ill=0.55,
+            pixel_size=6.5 / (40 * 1.4),
+        )
+        channel_2_Z, channel_2_Y, channel_2_X = channel_2_position[0][
+            0, channel_2_idx
+        ].shape
+        print(f"channel_2 focus idx: {focus_channel_2_idx}")
 
 
 @click.command()
@@ -43,55 +91,48 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
 
     channel_1_idx = int(input("Enter channel_1 index to process: "))
     channel_2_idx = int(input("Enter channel_2 index to process: "))
-    # %%
-    # TODO: get the microscope parameters through a yaml file? Currently hard-coded
-    # Find focus channel_1
-    print("Finding the channel_1 best focus position at t=0")
+
+    # Display volumes rescaled
     with open_ome_zarr(channel_1_data_path, mode="r") as channel_1_position:
         channel_1_str = channel_1_position.channel_names[channel_1_idx]
-        focus_channel_1_idx = focus_from_transverse_band(
-            channel_1_position[0][0, channel_1_idx],
-            NA_det=1.35,
-            lambda_ill=0.55,
-            pixel_size=3.45 / (46.2 * 1.4),
-        )
-        channel_1_Z, channel_1_Y, channel_1_X = channel_1_position[0][
-            0, channel_1_idx
-        ].shape
-        print(f"{channel_1_str} focus idx: {focus_channel_1_idx}")
-
-    # Find focus channel_2
-    print("Finding the channel_2 best focus position at t=0")
+        channel_1_volume = channel_1_position[0][0, channel_1_idx]
+        channel_1_Z, channel_1_Y, channel_1_X = channel_1_volume.shape
     with open_ome_zarr(channel_2_data_path, mode="r") as channel_2_position:
         channel_2_str = channel_2_position.channel_names[channel_2_idx]
-        focus_channel_2_idx = focus_from_transverse_band(
-            channel_2_position[0][0, channel_2_idx],
-            NA_det=1.35,
-            lambda_ill=0.55,
-            pixel_size=6.5 / (40 * 1.4),
-        )
-        channel_2_Z, channel_2_Y, channel_2_X = channel_2_position[0][
-            0, channel_2_idx
-        ].shape
-        print(f"channel_2 focus idx: {focus_channel_2_idx}")
+        channel_2_volume = channel_2_position[0][0, channel_2_idx]
+        channel_2_Z, channel_2_Y, channel_2_X = channel_2_volume.shape
 
-    # Display only the in-focus slices
-    channel_1_img = channel_1_position[0][
-        0, channel_1_idx, focus_channel_1_idx : focus_channel_1_idx + 1
-    ]
-    channel_2_img = channel_2_position[0][
-        0, channel_2_idx, focus_channel_2_idx : focus_channel_2_idx + 1
-    ]
-    viewer.add_image(channel_1_img, name=channel_1_str)
+    ## Find the z-scaling and apply it for display
+    # TODO: Get these values from the yaml file
+    # Use deskew metadta
+    try:
+        (
+            _,
+            _,
+            z_sampling_channel_2,
+            y_sampling_channel_2,
+            x_sampling_channel_2,
+        ) = channel_2_position.zattrs["multiscales"][0]["coordinateTransformations"][0][
+            "scale"
+        ]
+        voxel_size = (z_sampling_channel_2, y_sampling_channel_2, x_sampling_channel_2)
+    except LookupError("Couldn't find z_sampling in metadata"):
+        z_sampling_channel_2 = 1
+        voxel_size = (1, 1, 1)
+    magnification_LF_remote_volume = 1.4
+    step_size_LF_remote_volume = 0.4  # [um]
+    phase_remote_volume_sample_size = (
+        step_size_LF_remote_volume / magnification_LF_remote_volume
+    )
+    scaling_factor_z = phase_remote_volume_sample_size / z_sampling_channel_2
 
-    # TODO: Need to find a better way to handle rotations or just assume data is passed in the right orientation
-    if ROTATE_90deg_CCW:
-        channel_2_img = scipy.ndimage.rotate(
-            channel_2_img, angle=90, axes=(1, 2), reshape=True
-        )
-
-    layer_channel_2 = viewer.add_image(channel_2_img, name=channel_2_str)
-    layer_channel_2.translate = (0, 0, channel_1_X)
+    # Add layers to napari with and transform
+    # TODO: these rotations and scaling are not required for all datasets
+    layer_channel_1 = viewer.add_image(channel_1_volume, name=channel_1_str)
+    layer_channel_2 = viewer.add_image(channel_2_volume, name=channel_2_str)
+    layer_channel_1.scale = (scaling_factor_z, 1, 1)
+    layer_channel_2.rotate = 90
+    layer_channel_2.translate = (0, channel_2_X, channel_1_X)
 
     # %%
     # Manual annotation of features
@@ -110,6 +151,11 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
         if layer.mode == "add":
             if layer is points_channel_1:
                 next_layer = points_channel_2
+                # Change slider value
+                if len(next_layer.data) < 1:
+                    prev_step_channel_2 = viewer.dims.current_step
+                else:
+                    prev_step_channel_2 = (next_layer.data[-1][0] + 2, 0, 0)
                 # Add a point to the active layer
                 cursor_position = np.array(viewer.cursor.position)
                 layer.add(cursor_position)
@@ -119,21 +165,34 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
                 next_index = (current_index) % len(COLOR_CYCLE)
                 next_color = COLOR_CYCLE[next_index]
                 next_layer.current_face_color = next_color
+
+                # Switch to the next layer
+                next_layer.mode = "add"
+                layer.selected_data = {}
+                viewer.layers.selection.active = next_layer
+                viewer.dims.current_step = prev_step_channel_2
+
             else:
                 next_layer = points_channel_1
+                # Change slider value
+                if len(next_layer.data) < 1:
+                    prev_step_channel_1 = viewer.dims.current_step
+                else:
+                    # TODO: this +2 is not clear to me?
+                    prev_step_channel_1 = (next_layer.data[-1][0] + 2, 0, 0)
                 cursor_position = np.array(viewer.cursor.position)
                 layer.add(cursor_position)
-
                 # Change the colors
                 current_index = COLOR_CYCLE.index(layer.current_face_color)
                 next_index = (current_index + 1) % len(COLOR_CYCLE)
                 next_color = COLOR_CYCLE[next_index]
                 next_layer.current_face_color = next_color
 
-            # Switch to the next layer
-            next_layer.mode = "add"
-            layer.selected_data = {}
-            viewer.layers.selection.active = next_layer
+                # Switch to the next layer
+                next_layer.mode = "add"
+                layer.selected_data = {}
+                viewer.layers.selection.active = next_layer
+                viewer.dims.current_step = prev_step_channel_1
 
     # Create the first points layer
     points_channel_1 = viewer.add_points(
@@ -161,7 +220,7 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
 
     pts_channel_2[:, 2] -= channel_1_X  # subtract the translation offset for display
 
-    # Estimate the affine transform between the points in-focus
+    # Estimate the affine transform between the points xy to make sure registration is good
     transform = SimilarityTransform()
     transform.estimate(pts_channel_1[:, 1:], pts_channel_2[:, 1:])
     yx_points_transformation_matrix = transform.params
@@ -172,11 +231,6 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
         rotation_matrix = np.array(
             [[1, 0, 0, 0], [0, 0, 1, 0], [0, -1, 0, channel_2_X - 1], [0, 0, 0, 1]]
         )
-        # rotated_image = scipy.ndimage.affine_transform(
-        #     channel_2_position[0][0, channel_2_idx, focus_channel_2_idx : focus_channel_2_idx + 1],
-        #     rotation_matrix,
-        #     output_shape=(1, channel_2_X, channel_2_Y),
-        # )
     else:
         rotation_matrix = np.eye(4)
 
@@ -193,15 +247,20 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
     print(f"Affine Transform Matrix:\n {zyx_affine_transform}\n")
     output_shape_volume = (1, channel_1_Y, channel_1_X)
 
-    # Apply the affine transform to the image
+    # Demo: apply the affine transform to the image at the middle of the stack
     aligned_image = scipy.ndimage.affine_transform(
         channel_2_position[0][
-            0, channel_2_idx, focus_channel_2_idx : focus_channel_2_idx + 1
+            0, channel_2_idx, channel_2_Z // 2 : channel_2_Z // 2 + 1
         ],
         zyx_affine_transform,
         output_shape=output_shape_volume,
     )
-
+    viewer.add_image(
+        channel_1_position[0][
+            0, channel_1_idx, channel_1_Z // 2 : channel_1_Z // 2 + 1
+        ],
+        name=f"middle_plane_{channel_1_str}",
+    )
     viewer.add_image(aligned_image, name=f"registered_{channel_2_str}", opacity=0.5)
     viewer.layers[f"registered_{channel_2_str}"].colormap = "magenta"
     viewer.layers.remove(f"pts_{channel_1_str}")
@@ -217,25 +276,22 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
             _, _, z_sampling_channel_2, _, _ = channel_2_position.zattrs["multiscales"][
                 0
             ]["coordinateTransformations"][0]["scale"]
-        except:
+        except LookupError("Could not find coordinateTransformation scale in metadata"):
             z_sampling_channel_2 = 1
 
         print(z_sampling_channel_2)
-        # Scaling and translation factor based on metadata.
-        magnification_LF_remote_volume = 1.4
-        step_size_LF_remote_volume = 0.4  # [um]
-        phase_remote_volume_sample_size = (
-            step_size_LF_remote_volume / magnification_LF_remote_volume
+        # Estimate the Similarity Transform (rotation,scaling,translation)
+        transform = SimilarityTransform()
+        transform.estimate(pts_channel_1, pts_channel_2)
+        zyx_points_transformation_matrix = transform.params
+
+        z_scaling_matrix = np.array(
+            [[scaling_factor_z, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
         )
-        scaling_factor_z = phase_remote_volume_sample_size / z_sampling_channel_2
-        translation_z = (focus_channel_1_idx * scaling_factor_z) - focus_channel_2_idx
-        # 2D to 3D matrix
-        z_shift = np.array([scaling_factor_z, 0, 0, -translation_z])
-        print(f"Z-transform: {z_shift}")
-        zyx_points_transformation_matrix = np.vstack(
-            (z_shift, np.insert(yx_points_transformation_matrix, 0, 0, axis=1))
+        # Composite of all transforms
+        zyx_affine_transform = (
+            rotation_matrix @ zyx_points_transformation_matrix @ z_scaling_matrix
         )
-        zyx_affine_transform = rotation_matrix @ zyx_points_transformation_matrix
         print(f"Affine Transform Matrix:\n {zyx_affine_transform}\n")
 
         output_shape_volume = (channel_2_Z, channel_2_Y, channel_2_X)
@@ -255,6 +311,8 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
             colormap="magenta",
         )
         viewer.layers[f"registered_{channel_2_str}"].visible = False
+        viewer.layers[f"{channel_1_str}"].visible = False
+        viewer.layers[f"middle_plane_{channel_1_str}"].visible = False
 
     # Write and Save the matrix
     with open_ome_zarr(
@@ -270,13 +328,12 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
         registration_params = {
             "channel_1": channel_1_data_path,
             "channel_1_name": channel_1_str,
-            "channel_1_focused_slice": focus_channel_1_idx,
             "channel_2": channel_2_data_path,
             "channel_2_name": channel_2_str,
-            "channel_2_focused_slice": focus_channel_2_idx,
             "channel_2_90deg_CCW_rotation": ROTATE_90deg_CCW,
             "channel_1_shape": list((channel_1_Z, channel_1_Y, channel_1_X)),
             "channel_2_shape": list((channel_2_Z, channel_2_Y, channel_2_X)),
+            "voxel_size": list(voxel_size),
         }
         output_dataset.zattrs["registration"] = registration_params
 
@@ -287,5 +344,3 @@ def manual_registration(channel_1_data_path, channel_2_data_path, output_file):
 
 if __name__ == "__main__":
     manual_registration()
-
-# %%
