35d7bb8f3de36fcd8c773009ea29a42ef2d19a39
Talon Chandler <talonchandler@gmail.com>
2023-08-05 12:29:01 -0700

    show `mantis -h` in order

--- mantis/cli/main.py
+++ mantis/cli/main.py
@@ -10,14 +10,20 @@ from mantis.cli.run_acquisition import run_acquisition
 CONTEXT = {"help_option_names": ["-h", "--help"]}
 
 
-@click.group(context_settings=CONTEXT)
+# `mantis -h` will show subcommands in the order they are added
+class NaturalOrderGroup(click.Group):
+    def list_commands(self, ctx):
+        return self.commands.keys()
+
+
+@click.group(context_settings=CONTEXT, cls=NaturalOrderGroup)
 def cli():
     """command-line tools for mantis"""
 
 
-cli.add_command(deskew)
+cli.add_command(run_acquisition)
 cli.add_command(estimate_bleaching)
 cli.add_command(estimate_deskew)
-cli.add_command(run_acquisition)
+cli.add_command(deskew)
 cli.add_command(estimate_registration)
 cli.add_command(register)
