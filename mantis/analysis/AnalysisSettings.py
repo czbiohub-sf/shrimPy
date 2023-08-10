9e39a5563b58b7e6caf91d8eca782ccd06d4bd54
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2023-08-09 18:02:04 -0700

    reducing parameters in config file

--- mantis/analysis/AnalysisSettings.py
+++ mantis/analysis/AnalysisSettings.py
@@ -5,7 +5,7 @@ import numpy as np
 from pydantic import ConfigDict, PositiveFloat, PositiveInt, validator
 from pydantic.dataclasses import dataclass
 
-config = ConfigDict(extra='forbid')
+config = ConfigDict(extra="forbid")
 
 
 @dataclass(config=config)
@@ -34,36 +34,18 @@ class DeskewSettings:
                 self.px_to_scan_ratio = round(self.pixel_size_um / self.scan_step_um, 3)
             else:
                 raise TypeError("px_to_scan_ratio is not valid")
-config = ConfigDict(extra='forbid')
+
+
+config = ConfigDict(extra="forbid")
+
 
 @dataclass(config=config)
 class RegistrationSettings:
     affine_transform_zyx: list
-    voxel_size: list
     output_shape: list
-    pts_phase_registration: list = None
-    pts_fluor_registration: list = None
-    phase_channel_path: str = None
-    fluor_channel_path: str = None
-    fluor_channel_90deg_CCW_rotation: bool = True
+    k_90deg_rot: int = 0
 
-    def validate_nx3_array(v):
-        if not isinstance(v, list) or len(v) < 3:
-            raise ValueError("The input array must be a list with at least 3 rows.")
-        for row in v:
-            if not isinstance(row, list) or len(row) != 3:
-                raise ValueError("Each row of the array must be a list of length 3.")
-
-        return v
-
-    _validate_pts_phase_registration = validator('pts_phase_registration', allow_reuse=True)(
-        validate_nx3_array
-    )
-    _validate_pts_fluor_registration = validator('pts_fluor_registration', allow_reuse=True)(
-        validate_nx3_array
-    )
-
-    @validator('affine_transform_zyx')
+    @validator("affine_transform_zyx")
     def check_affine_transform(cls, v):
         if not isinstance(v, list) or len(v) != 4:
             raise ValueError("The input array must be a list of length 3.")
@@ -80,13 +62,4 @@ class RegistrationSettings:
         except ValueError:
             raise ValueError("The array must contain valid numerical values.")
 
-        return v
-
-    @validator('pts_phase_registration')
-    def validate_same_entries(cls, v, values, **kwargs):
-        array_data2 = values.get('pts_fluor_registration')
-        if array_data2 and len(v) != len(array_data2):
-            raise ValueError(
-                "'pts_phase_registration' and 'pts_fluor_registration' must have the same number of entries."
-            )
         return v
