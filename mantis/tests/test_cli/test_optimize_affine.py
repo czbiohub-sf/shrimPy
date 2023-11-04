9bfbd20a75d63c693f56ed570c506fd9b0cba50d
Talon Chandler <talonchandler@gmail.com>
2023-11-03 17:03:04 -0700

    test fix

--- mantis/tests/test_cli/test_optimize_affine.py
+++ mantis/tests/test_cli/test_optimize_affine.py
@@ -3,9 +3,9 @@ from click.testing import CliRunner
 from mantis.cli.main import cli
 
 
-def test_optimize_affine_cli(tmp_path, example_plate, example_estimate_affine_settings):
+def test_optimize_affine_cli(tmp_path, example_plate, example_apply_affine_settings):
     plate_path, _ = example_plate
-    config_path, _ = example_estimate_affine_settings
+    config_path, _ = example_apply_affine_settings
     output_path = tmp_path / "config.yaml"
 
     runner = CliRunner()
@@ -25,4 +25,4 @@ def test_optimize_affine_cli(tmp_path, example_plate, example_estimate_affine_se
     )
 
     # Weak test
-    assert "Enter phase_channel index to process" in result.output
+    assert "Getting dataset info" in result.output
