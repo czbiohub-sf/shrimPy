81f397733929d7a184a718b72628ddd8c2cd5fca
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2023-11-07 17:15:21 -0800

    debug hook functions for better acq abort

--- mantis/acquisition/hook_functions/globals.py
+++ mantis/acquisition/hook_functions/globals.py
@@ -1,8 +1,10 @@
 ls_last_img_idx = {'position': 0, 'time': 0, 'channel': '', 'z': 0}
 ls_acq_finished = True
+ls_acq_aborted = False
 
 lf_last_img_idx = {'position': 0, 'time': 0, 'channel': '', 'z': 0}
 lf_acq_finished = True
+lf_acq_aborted = False
 
 ls_slice_acquisition_rates = None
 ls_laser_powers = None
