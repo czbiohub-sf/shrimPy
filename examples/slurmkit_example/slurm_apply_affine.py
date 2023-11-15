df53a7458ea3763c95e69d825126c52b90729bcf
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2023-11-14 17:03:56 -0800

    testing slurmkit

--- examples/slurmkit_example/slurm_apply_affine.py
+++ examples/slurmkit_example/slurm_apply_affine.py
@@ -5,14 +5,18 @@ from mantis.cli import utils
 from slurmkit import SlurmParams, slurm_function, submit_function
 from natsort import natsorted
 import click
-from mantis.cli.apply_affine import registration_params_from_file, rotate_n_affine_transform
 import numpy as np
 from iohub import open_ome_zarr
-
+from mantis.analysis.AnalysisSettings import RegistrationSettings
+from pathlib import Path
+from typing import List
+from mantis.cli.parsing import config_filepath, input_position_dirpaths, output_dirpath
+from mantis.cli.utils import yaml_to_model
+from mantis.cli.apply_affine import apply_affine_to_scale
 
 # io parameters
-labelfree_data_paths = '/hpc/projects/comp.micro/mantis/2023_08_09_HEK_PCNA_H2B/2-phase3D/pcna_rac1_virtual_staining_b1_redo_1/phase3D.zarr/0/0/0'
-lightsheet_data_paths = '/hpc/projects/comp.micro/mantis/2023_08_09_HEK_PCNA_H2B/1-deskew/pcna_rac1_virtual_staining_b1_redo_1/deskewed.zarr/0/0/0'
+source_data_paths = '/hpc/projects/comp.micro/mantis/2023_08_09_HEK_PCNA_H2B/2-phase3D/pcna_rac1_virtual_staining_b1_redo_1/phase3D.zarr/0/0/0'
+target_data_paths = '/hpc/projects/comp.micro/mantis/2023_08_09_HEK_PCNA_H2B/1-deskew/pcna_rac1_virtual_staining_b1_redo_1/deskewed.zarr/0/0/0'
 output_data_path = './registered_output.zarr'
 registration_param_path = './register.yml'
 
@@ -23,26 +27,27 @@ time = 40  # minutes
 simultaneous_processes_per_node = 5
 
 # path handling
-labelfree_data_paths = natsorted(glob.glob(labelfree_data_paths))
-lightsheet_data_paths = natsorted(glob.glob(lightsheet_data_paths))
+source_data_paths = natsorted(glob.glob(source_data_paths))
+target_data_paths = natsorted(glob.glob(target_data_paths))
 output_dir = os.path.dirname(output_data_path)
-output_paths = utils.get_output_paths(labelfree_data_paths, output_data_path)
-click.echo(f"in: {labelfree_data_paths}, out: {output_paths}")
+output_paths = utils.get_output_paths(source_data_paths, output_data_path)
+click.echo(f"in: {source_data_paths}, out: {output_paths}")
 slurm_out_path = str(os.path.join(output_dir, "slurm_output/register-%j.out"))
 
 # Additional registraion arguments
 # Parse from the yaml file
-settings = registration_params_from_file(registration_param_path)
+settings = yaml_to_model(config_filepath, RegistrationSettings)
 matrix = np.array(settings.affine_transform_zyx)
 output_shape_zyx = tuple(settings.output_shape_zyx)
 
-# Get the output voxel_size
-with open_ome_zarr(lightsheet_data_paths[0]) as light_sheet_position:
-    voxel_size = tuple(light_sheet_position.scale[-3:])
+# Calculate the output voxel size from the input scale and affine transform
+with open_ome_zarr(input_position_dirpaths[0]) as input_dataset:
+    output_voxel_size = apply_affine_to_scale(matrix[:3, :3], input_dataset.scale[-3:])
 
+# Get the affine transformation matrix
 extra_metadata = {
-    'registration': {
-        'affine_matrix': matrix.tolist(),
+    'affine_transformation': {
+        'transform_matrix': matrix.tolist(),
     }
 }
 affine_transform_args = {
@@ -51,7 +56,7 @@ affine_transform_args = {
     'extra_metadata': extra_metadata,
 }
 utils.create_empty_zarr(
-    position_paths=labelfree_data_paths,
+    position_paths=source_data_paths,
     output_path=output_data_path,
     output_zyx_shape=output_shape_zyx,
     chunk_zyx_shape=None,
@@ -70,7 +75,7 @@ params = SlurmParams(
 # wrap our utils.process_single_position() function with slurmkit
 slurm_process_single_position = slurm_function(utils.process_single_position)
 register_func = slurm_process_single_position(
-    func=rotate_n_affine_transform,
+    func=utils.affine_transform,
     num_processes=simultaneous_processes_per_node,
     **affine_transform_args,
 )
@@ -83,5 +88,5 @@ register_jobs = [
         input_data_path=in_path,
         output_path=out_path,
     )
-    for in_path, out_path in zip(labelfree_data_paths, output_paths)
+    for in_path, out_path in zip(source_data_paths, output_paths)
 ]
