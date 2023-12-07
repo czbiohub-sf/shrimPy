fixing the xy drift calculation. refactoring placed xy order instead of yx

mantis/cli/stabilization.py
@@ -198,13 +198,13 @@ def calculate_z_drift(
 
 
 def calculate_yx_stabilization(
-    input_data_path: Path,
+    input_data_paths: Path,
     output_folder_path: Path,
     c_idx: int = 0,
     crop_size_xy: list[int, int] = (400, 400),
     verbose: bool = False,
 ) -> np.ndarray:
-    input_position = open_ome_zarr(input_data_path[0])
+    input_position = open_ome_zarr(input_data_paths[0])
     output_folder_path = Path(output_folder_path)
     output_folder_path.mkdir(parents=True, exist_ok=True)
 
@@ -243,6 +243,11 @@ def calculate_yx_stabilization(
 
     print("Finding XY translation matrices")
     T_stackreg = sr.register_stack(xy_timelapse, reference="previous", axis=0)
+
+    # Swap values in the array since stackreg is xy and we need yx
+    for subarray in T_stackreg:
+        subarray[0, 2], subarray[1, 2] = subarray[1, 2], subarray[0, 2]
+
     T_zyx_shift = np.zeros((T_stackreg.shape[0], 4, 4))
     T_zyx_shift[:, 1:4, 1:4] = T_stackreg
     T_zyx_shift[:, 0, 0] = 1
@@ -353,7 +358,7 @@ def estimate_stabilization_affine_list(
     # Estimate yx drift
     if estimate_yx_drift:
         T_translation_mats = calculate_yx_stabilization(
-            input_data_path=input_position_dirpaths,
+            input_data_paths=input_position_dirpaths,
             output_folder_path=output_dirpath,
             c_idx=channel_index,
             crop_size_xy=crop_size_xy,
@@ -416,12 +421,13 @@ def stabilize_timelapse(
 
     combined_mats = settings.affine_transform_zyx_list
     combined_mats = np.array(combined_mats)
+    print(combined_mats.shape)
+    print(combined_mats[0].shape)
 
     with open_ome_zarr(input_position_dirpaths[0]) as dataset:
         T, C, Z, Y, X = dataset.data.shape
         channel_names = dataset.channel_names
-    C = 1
-    T = 5
+
     chunk_zyx_shape = (Z_CHUNK, Y, X)
     output_metadata = {
         "shape": (T, C, Z, Y, X),
@@ -445,7 +451,7 @@ def stabilize_timelapse(
             input_data_path=input_path,
             output_path=output_dirpath,
             time_indices=list(range(T)),
-            input_channel_idx=[4],
-            output_channel_idx=[0],
+            input_channel_idx=None,
+            output_channel_idx=None,
             num_processes=num_processes,
         )

mantis/cli/utils.py
@@ -550,7 +550,7 @@ def append_channels(input_data_path: Path, target_data_path: Path) -> None:
     appending_dataset.close()
 
 
-def numpy_to_ants_transform_zyx(T_numpy):
+def numpy_to_ants_transform_zyx(T_numpy: np.ndarray):
     """Homogeneous 3D transformation matrix from numpy to ants
 
     Parameters
@@ -648,7 +648,8 @@ def apply_stabilization_over_time_ants(
             input_dataset,
             output_path / Path(*input_data_path.parts[-3:]),
             list_of_shifts,
-            input_channel_indices=None,
+            None,
+            None,
         )
     else:
         # If C is empty, use only the range for time_indices
@@ -674,7 +675,7 @@ def apply_stabilization_over_time_ants(
 def stabilization_over_time_ants(
     position: Position,
     output_path: Path,
-    list_of_shifts: np.array,
+    list_of_shifts: np.ndarray,
     input_channel_idx: list,
     output_channel_idx: list,
     t_idx: int,
@@ -682,8 +683,8 @@ def stabilization_over_time_ants(
     **kwargs,
 ) -> None:
     """Load a zyx array from a Position object, apply a transformation and save the result to file"""
-    click.echo(f"Processing c={c_idx}, t={t_idx}")
 
+    click.echo(f"Processing c={c_idx}, t={t_idx}")
     tx_shifts = numpy_to_ants_transform_zyx(list_of_shifts[t_idx])
 
     # Process CZYX vs ZYX
@@ -692,7 +693,7 @@ def stabilization_over_time_ants(
         if not _check_nan_n_zeros(czyx_data):
             for c in input_channel_idx:
                 print(f'czyx_data.shape {czyx_data.shape}')
-                zyx_data_ants = numpy_to_ants_transform_zyx(czyx_data[0])
+                zyx_data_ants = ants.from_numpy(czyx_data[0])
                 registered_zyx = tx_shifts.apply_to_image(
                     zyx_data_ants, reference=zyx_data_ants
                 )
@@ -706,10 +707,9 @@ def stabilization_over_time_ants(
             click.echo(f"Skipping t={t_idx} due to all zeros or nans")
     else:
         zyx_data = position.data.oindex[t_idx, c_idx]
-
         # Checking if nans or zeros and skip processing
         if not _check_nan_n_zeros(zyx_data):
-            zyx_data_ants = numpy_to_ants_transform_zyx(zyx_data)
+            zyx_data_ants = ants.from_numpy(zyx_data)
             # Apply transformation
             registered_zyx = tx_shifts.apply_to_image(zyx_data_ants, reference=zyx_data_ants)
 