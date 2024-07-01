13f0d4b62757135bc1134226860502cf0ad42465
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2024-07-01 15:41:31 -0700

    update slurmkit_stabilize example

--- examples/slurmkit_example/slurmkit_stabilize.py
+++ examples/slurmkit_example/slurmkit_stabilize.py
@@ -7,28 +7,26 @@ from iohub import open_ome_zarr
 from pathlib import Path
 from mantis.cli.utils import (
     yaml_to_model,
-    create_empty_hcs_zarr
+    create_empty_hcs_zarr,
+    process_single_position_v2,
 )
-from mantis.cli.stabilize import apply_stabilization_over_time_ants
+from mantis.cli.stabilize import apply_stabilization_transform
 import numpy as np
 from mantis.analysis.AnalysisSettings import StabilizationSettings
 
 # NOTE: this pipeline uses the focus found on well one for all. Perhaps this should be done per FOV(?)
 
 # io parameters
-input_position_dirpaths = "/hpc/projects/comp.micro/mantis/2023_09_22_A549_0.52NA_illum/4.1-virutal-staining-v2/A549_MitoViewGreen_LysoTracker_W3_FOV5_1_phase_VS.zarr/0/FOV0/0"
-output_dirpath = "./test_stabilized.zarr"
-config_filepath = "/hpc/projects/comp.micro/mantis/2023_09_22_A549_0.52NA_illum/4.1-virutal-staining-v2/test_stabilization.yml"
+input_position_dirpaths = "/hpc/projects/comp.micro/mantis/2023_11_08_Opencell_infection_redo/1-recon/OC43_infection_timelapse_3.zarr/*/*/*"
+output_dirpath = "/hpc/projects/comp.micro/mantis/2023_11_08_Opencell_infection_redo/3-stabilization/OC43_infection_timelapse_3.zarr"
+config_filepath = "/hpc/projects/comp.micro/mantis/2023_11_08_Opencell_infection_redo/3-stabilization/stabilization.yml"
 
 # batch and resource parameters
 partition = 'preempted'
-cpus_per_task = 8
+cpus_per_task = 5
 mem_per_cpu = "4G"
-time = 300  # minutes
-simultaneous_processes_per_node = 4
-slurmkit_array_chunk = 20
-Z_CHUNK=20
-
+time = 60  # minutes
+simultaneous_processes_per_node = 5
 
 # convert to Path
 input_position_dirpaths = [Path(p) for p in natsorted(glob.glob(input_position_dirpaths))]
@@ -43,10 +41,8 @@ with open_ome_zarr(input_position_dirpaths[0]) as dataset:
     T, C, Z, Y, X = dataset.data.shape
     channel_names = dataset.channel_names
 
-chunk_zyx_shape = (Z_CHUNK, Y, X)
 output_metadata = {
     "shape": (T, C, Z, Y, X),
-    "chunks": (1,) * 2 + chunk_zyx_shape,
     "scale": dataset.scale,
     "channel_names": channel_names,
     "dtype": np.float32,
@@ -71,40 +67,22 @@ params = SlurmParams(
 )
 
 # wrap our utils.process_single_position() function with slurmkit
-slurm_process_single_position = slurm_function(apply_stabilization_over_time_ants)
+slurm_process_single_position = slurm_function(process_single_position_v2)
 stabilization_function = slurm_process_single_position(
+    func=apply_stabilization_transform,
     list_of_shifts=combined_mats,
+    time_indices=list(range(T)),
+    input_channel_idx=None,
+    output_channel_idx=None,
     num_processes=simultaneous_processes_per_node,
-    time_indices = list(range(T)),
-    input_channel_idx = None,
-    output_channel_idx = None
 )
 
-# Making batches of jobs to avoid IO overload
-
-stabilization_jobs = []
-for i in range(0, len(input_position_dirpaths), slurmkit_array_chunk):
-    chunk_input_paths = input_position_dirpaths[i : i + slurmkit_array_chunk]
-
-    if i == 0:
-        stabilization_jobs = [
-            submit_function(
-                stabilization_function,
-                slurm_params=params,
-                input_data_path=in_path,
-                output_path=output_dirpath,
-            )
-            for in_path in chunk_input_paths
-        ]
-
-    else:
-        stabilization_jobs = [
-            submit_function(
-                stabilization_function,
-                slurm_params=params,
-                input_data_path=in_path,
-                output_path=output_dirpath,
-                dependencies=stabilization_jobs,
-            )
-            for in_path in chunk_input_paths
-        ]
+stabilization_jobs = [
+    submit_function(
+        stabilization_function,
+        slurm_params=params,
+        input_data_path=in_path,
+        output_path=output_dirpath,
+    )
+    for in_path in input_position_dirpaths
+]
