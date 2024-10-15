53d6754d98ce93df457ea0c459ed476bc4ac4228
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2024-10-14 18:58:45 -0700

    remove comments

--- mantis/tests/test_cli/test_concatenate_cli.py
+++ mantis/tests/test_cli/test_concatenate_cli.py
@@ -5,7 +5,7 @@ from click.testing import CliRunner
 from mantis.cli.main import cli
 
 
-def test_concatenate_cli(example_plate, tmp_path, example_concatenate_settings, capsys):
+def test_concatenate_cli(example_plate, tmp_path, example_concatenate_settings):
     plate_path_1, _ = example_plate
     settings_path, settings = example_concatenate_settings
     config_path = tmp_path / "concat.yml"
@@ -42,20 +42,3 @@ def test_concatenate_cli(example_plate, tmp_path, example_concatenate_settings, 
     assert 'deprecated' in result.output.lower()
     assert output_path.exists()
     assert result.exit_code == 0
-
-
-# def test_deprecated(capsys):
-#     runner = CliRunner()
-#     result = runner.invoke(
-#         cli,
-#         [
-#             "concatenate",
-#             "-h",
-#         ],
-#     )
-#     captured = capsys.readouterr()
-
-#     assert "Deprecated" in captured.out
-
-#     captured = capsys.readouterr()
-#     assert 'biahub.' in captured.err
