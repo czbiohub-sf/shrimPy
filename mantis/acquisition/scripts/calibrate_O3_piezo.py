02562348bbc952caefa2a09da9d81820637c18b5
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2024-10-15 17:40:39 -0700

    expand O3 calibration script

--- mantis/acquisition/scripts/calibrate_O3_piezo.py
+++ mantis/acquisition/scripts/calibrate_O3_piezo.py
@@ -14,21 +14,38 @@ import matplotlib.pyplot as plt
 
 LS_KIM101_SN = 74000291
 
+mmc = Core()
+mmc.set_roi(768, 768, 512, 512)
+
 def smooth(data, window_size=5):
     cumsum_vec = np.cumsum(np.insert(data, 0, 0))
     data = (cumsum_vec[window_size:] - cumsum_vec[:-window_size]) / window_size
     return data
+
+def snap_image(mmc) -> np.ndarray:
+    mmc.snap_image()
+    tagged_image = mmc.get_tagged_image()
+
+    # get image data
+    image_data = np.reshape(
+        tagged_image.pix, (tagged_image.tags['Height'], tagged_image.tags['Width'])
+    )
+    
+    return image_data.astype('uint16')
+
 
 # %% Calibrate  background correction distance
 # Acquire a number of defocus stacks and check that the stack starting and ending
 # points don't drift up or down. Adjust the backlash correction distance if needed.
 # This step is more important than adjusting the compensation factor below.
 
-mmc = Core()
 o3_stage = setup_kim101_stage(LS_KIM101_SN)
 
+img0 = snap_image(mmc)
+peak0 = np.argmax(smooth(img0.std(axis=-1), window_size=5))
+
 repeats = 10
-z_range = np.arange(-165, 165, 15)
+z_range = np.arange(-165, 165+15, 15)
 data = []
 for _ in range(repeats):
     data.append(
@@ -36,7 +53,7 @@ for _ in range(repeats):
             mmc,
             z_stage=o3_stage,
             z_range=z_range,
-            backlash_correction_distance=10,
+            backlash_correction_distance=0,
         )
     )
 data = np.concatenate(data)
@@ -46,8 +63,12 @@ data_std = data.std(axis=-1)
 peaks = []
 for d in data_std:
     peaks.append(np.argmax(smooth(d, window_size=5)))
+n = len(z_range)
+z0_std = peaks[n//2::n]
 
 plt.plot(peaks)
+plt.axhline(y=peak0, color='r', linestyle='--')
+plt.plot(np.arange(n//2, n*repeats, n), z0_std, 'o')
 plt.grid()
 
 # %% Calibrate compensation factor
