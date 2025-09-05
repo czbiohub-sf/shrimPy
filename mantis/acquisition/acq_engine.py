6117af81f33cc0437a512b4249086f5d2c88fcf5
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2025-09-04 21:30:17 -0700

    set o3 stage position at beginning of new acquisition

--- mantis/acquisition/acq_engine.py
+++ mantis/acquisition/acq_engine.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 import time
 
 from copy import deepcopy
@@ -439,6 +440,11 @@ class MantisAcquisition(object):
         self.close()
 
     def close(self):
+        # Log final O3 stage position
+        if self.ls_acq.o3_stage:
+            _pos = float(self.ls_acq.mmc.get_position(self.ls_acq.o3_stage))
+            logger.debug(f'Final O3 stage position: {_pos:.3f} um')
+
         # Shut down DAQ
         if not self._demo_run:
             self.cleanup_daq()
@@ -735,6 +741,44 @@ class MantisAcquisition(object):
                             ts2_ttl_state, config_name, config_group
                         )
                     )
+
+    def setup_o3_refocus(self):
+        """
+        The position of the O3 refocus stage resets between chunks in a chunked acquisition.
+        Here we'll read the final position of the O3 refocus stage from the log file of the
+        previous chunk and set the O3 refocus stage to that position at the start of the current chunk.
+        """
+        if not self.ls_acq.microscope_settings.use_o3_refocus:
+            return
+
+        acq_name_parts = self._acq_dir.name.split('_')
+        acq_name = '_'.join(acq_name_parts[:-1])
+        prev_chunk = int(acq_name_parts[-1]) - 1
+        prev_logs_dir = self._root_dir / f'{acq_name}_{prev_chunk}' / 'logs'
+        prev_log_file = list(prev_logs_dir.glob('mantis_acquisition_log_*.txt'))
+        if not prev_log_file:
+            logger.debug(
+                'No log files from a previous acquisition found. Will not change O3 stage position.'
+            )
+            return
+
+        o3_position = None
+        with open(prev_log_file[0], 'r') as f:
+            for line in reversed(f.readlines()):
+                match = re.search(r"Final O3 stage position: ([-\d\.]+) um", line)
+                if match:
+                    o3_position = float(match.group(1))
+                    break
+        if o3_position is None:
+            logger.debug(
+                "The final O3 stage position was not found in the previous log file. Will not change O3 stage position."
+            )
+            return
+
+        logger.debug(f'Updating O3 stage position to {o3_position:.3f} um')
+        microscope_operations.set_z_position(
+            self.ls_acq.mmc, self.ls_acq.o3_stage, o3_position
+        )
 
     def go_to_position(self, position_index: int):
         # Move slowly for short distances such that autofocus can stay engaged.
@@ -1128,6 +1172,9 @@ class MantisAcquisition(object):
         logger.debug('Setting up autoexposure')
         self.setup_autoexposure()
 
+        logger.debug('Setting up O3 refocus')
+        self.setup_o3_refocus()
+
     def acquire(self):
         """
         Simultaneously acquire label-free and light-sheet data over multiple
@@ -1448,7 +1495,7 @@ def _generate_channel_slice_acq_events(
 
 
 def _create_acquisition_directory(root_dir: Path, acq_name: str, idx=1) -> Path:
-    acq_dir = root_dir / f'{acq_name}_{idx}'
+    acq_dir = Path(root_dir) / f'{acq_name}_{idx}'
     # 10000 4 GB files would be 40 TB, which should be plenty
     _ndtif_filename = (
         acq_dir
