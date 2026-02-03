af80e0f260e50f9ee54830c6c005bef6e50170fd
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2026-02-02 16:47:23 -0800

    use setuptools_scm for version management

--- shrimpy/__init__.py
+++ shrimpy/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "0.2.0"
+"""shrimpy - Custom acquisition engines for optical microscopes."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("shrimpy")
+except PackageNotFoundError:
+    # Package is not installed
+    __version__ = "unknown"
