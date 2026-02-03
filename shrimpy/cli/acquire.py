f7515ee0f39b774587f3f1509e1c3404fa34218a
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2026-02-03 10:00:52 -0800

    leave note to update CLI args

--- shrimpy/cli/acquire.py
+++ shrimpy/cli/acquire.py
@@ -13,6 +13,9 @@ def acquire():
     pass
 
 
+# TODO: update cli arguments, for example mm-config instead of mmconfig,
+# path instead of save-dir, name instead of acquisition-name,
+# acq-config instead of mda-sequence, etc.
 @acquire.command()
 @click.option(
     "--mmconfig",
