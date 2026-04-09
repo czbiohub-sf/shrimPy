d09b23226dcb88ee981880037fd792d9ea98763d
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2026-04-08 17:18:34 -0700

    set pymmcore-plus env vars

--- shrimpy/__init__.py
+++ shrimpy/__init__.py
@@ -1,5 +1,7 @@
 """shrimpy - Custom acquisition engines for optical microscopes."""
 
+import os
+
 from importlib.metadata import PackageNotFoundError, version
 
 try:
@@ -7,3 +9,7 @@ try:
 except PackageNotFoundError:
     # Package is not installed
     __version__ = "unknown"
+
+# Set pymmcore-plus circular buffer memory to 2048 MB
+if not os.environ.get("PYMM_BUFFER_SIZE_MB"):
+    os.environ["PYMM_BUFFER_SIZE_MB"] = "2048"
